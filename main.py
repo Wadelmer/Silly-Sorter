9820a1bf56f39bdc451abb07fec91e8ae8ef9889
TextBox75 <53096974+TextBox75@users.noreply.github.com>
2024-11-18 22:21:49 +0000

    Implemented operations

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import os
+import operator
 
-input_dict = {}
+def collectdata():
+    key_len = 0
+    input_dict = {}
+    while True:
+        for i, v in input_dict.items():
+            print(f"{i:{key_len}<}:{v:>10}") # For loop that prints current keys and values in the dictionary.
+        key = input("Insert key: ")
+        if key == '':
+            break                            # If your input is empty, the program will assume you want to stop.
+        key_len = max(len(key), key_len)     # Getting the maximum length of the keys so the print looks consistent.
+        value = input("Insert value: ")
+        try:
+            input_dict[key] = float(value)   # This line tries to convert a string to a float, if it's not possible...
+        except ValueError:
+            print("That's not a number!")    # It notifies you!
+    return input_dict, key_len               # Return collected dictionary and largest key length
 
-key_len = 0
-while True:
-    os.system('clear') # This function is used to clear the terminal.
-    for i, v in input_dict.items():
-        print(f"{i:{key_len}<}:{v:>10}") # For loop that prints current keys and values in the dictionary.
-    key = input()
-    if key == '':
-        break # If your input is empty, the program will assume you want to stop.
-    key_len = max(len(key), key_len) # Getting the maximum length of the keys so the print looks consistent.
-    value = input()
-    try:
-        input_dict[key] = float(value) # This line tries to convert a string to a float, if it's not possible...
-    except ValueError:
-        print("That's not a number!") # It notifies you!
+def filter(input_dict, key_len):
+    os.system('cls')
+    op = input("Enter operation (>, >= or <, <=): ")
+    cond = input("Enter condition: ")
 
-# sorted(input_dict.items(), reverse=True)
+    operators = {">": operator.gt, "<": operator.lt, ">=": operator.ge, "<=": operator.le} # String-operator look-up dictionary
+    filteredata = {}
+    for i, v in input_dict.items():         # For each item in dictionary
+        if operators[op](v, float(cond)):   # Check if item value complies with specified operation and condition
+            filteredata[i] = v              # Append item-value pair to filtered data if so
+    
+    print("------------------------------")
+    for i, v in filteredata.items():        # For each item in filtered dictionary
+        print(f"{i:{key_len}<}:{v:>10}")    # Print item-value pair in formatted fashion
+    print("------------------------------")
+    print(f"Size of filtered data: {len(filteredata)}")
+
+if __name__ == "__main__":
+    args = collectdata()
+    filter(args[0], args[1]) # Pass first function's return arguments into second function
